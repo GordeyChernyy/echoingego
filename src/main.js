c4e5a18c5f7d2c2eb48768096cdedbcfc61da190
gordey <kewava@gmail.com>
2016-06-17 10:06:35 -0400

    osc-web

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 'use strict';
-var oscPort = new osc.WebSocketPort({
-    url: "ws://localhost:8000" // URL to your Web Socket server.
-});
-oscPort.open();
-oscPort.on("message", function (oscMsg) {
-    console.log("An OSC message just arrived!", oscMsg);
-});
+
+//-------------------- OSC 
+
+
+//-------------------- Paper
 var layer = new paper.Layer();
 var group = new paper.Group();
 
@@ -14,3 +12,7 @@ group.transformContent = false;
 paper.project.importSVG("assets/test.svg", function(item) {
     group.addChild(item);
 });
+
+function onFrame(event) {
+	
+}
