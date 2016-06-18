0099bb6dcc33c5a9a65d07d8dc213b4322d1311e
gordey <kewava@gmail.com>
2016-06-18 14:46:13 -0400

    fix websocket connection

--- src/main.js
+++ src/main.js
@@ -14,8 +14,15 @@ paper.project.importSVG("assets/test.svg", function(item) {
     group.scaling = [0.3, 0.3];
 });
 var counter = 0;
-function onFrame(event) {
-	group.position = [window.skeletonPositionData["/righthand_pos"].x, window.skeletonPositionData["/righthand_pos"].y];
-	// console.log(window.data);
 
+function onFrame(event) {
+	
+	if(window.skeletonPositionData != undefined && window.skeletonPositionData != undefined){
+		if(window.skeletonPositionData["/righthand_pos"]!= undefined && window.skeletonPositionData["/righthand_pos"] != undefined){
+			group.position = [window.skeletonPositionData["/righthand_pos"]["x"], window.skeletonPositionData["/righthand_pos"]["y"]];
+		}
+	}
+
+	counter++;
+	// console.log();
 }
