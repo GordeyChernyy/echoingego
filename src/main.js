9949b3d1310913c9c3bbc2f55139ec84130828e5
gordey <kewava@gmail.com>
2016-06-18 12:49:31 -0400

    split osc data

--- src/main.js
+++ src/main.js
@@ -11,8 +11,11 @@ group.transformContent = false;
 
 paper.project.importSVG("assets/test.svg", function(item) {
     group.addChild(item);
+    group.scaling = [0.3, 0.3];
 });
-
+var counter = 0;
 function onFrame(event) {
-	
+	group.position = [window.skeletonPositionData["/righthand_pos"].x, window.skeletonPositionData["/righthand_pos"].y];
+	// console.log(window.data);
+
 }
